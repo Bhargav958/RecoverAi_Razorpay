0f4ff4dbd0edc2418ed87d8c19b1e36ceb14f457
Bhargav958 <pallebhargav0705@gmail.com>
2026-08-25 23:10:14 +0530

    Added the requried api keys inside the env file and initialized the app.js and serber.js files

---
+++ README.md
@@ -0,0 +1,3 @@
+RecoverAi idea come from a very important problems facing by the merchants in cooperation with RazorPay.
+It is building to make the merchants to know about the payments flaws and others.
+
---
+++ backend/src/app.js
@@ -0,0 +1,20 @@
+import express from "express"
+import cors from "cors"
+
+const app = express()
+
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials: true
+}));
+
+app.use(express.json())
+
+app.get("/api/health", (req,res) =>{ 
+    res.json({
+        success: true,
+        message: "RecoverAI backend is running"
+    });
+});
+
+export default app
